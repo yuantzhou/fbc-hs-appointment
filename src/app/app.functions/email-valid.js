182ac216d794f20fc3fdd38095e174de6a8194f9
yuantzhou <46725037+yuantzhou@users.noreply.github.com>
2025-09-15 01:27:12 -0600

    Update email-valid.js
    remove email validation key

--- src/app/app.functions/email-valid.js
+++ src/app/app.functions/email-valid.js
@@ -7,7 +7,7 @@ exports.main = async (context = {}) => {
 //       console.log(Object.keys(emailValidresponse))
 //       console.log(emailValidresponse.data)
 // return emailValidresponse.data
-const response= await axios.get(`https://emailreputation.abstractapi.com/v1/?api_key=4b060da399904ff4ae316ac8f3105346&email=${context.parameters.email}`)
+const response= await axios.get(`https://emailreputation.abstractapi.com/v1/?api_key=APIKEY&email=${context.parameters.email}`)
                  
       return response.data
 }
